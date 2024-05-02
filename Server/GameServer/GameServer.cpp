79460d52f4243b9a77da7c15c5cd7278de5c4573
DeukYu <ldy8070@naver.com>
2024-05-02 21:40:35 +0900

    Update GameServer.cpp

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,32 +1,25 @@
 ﻿#include "pch.h"
 #include <thread>
+#include <mutex>
 
-atomic<int32> sum = 0;
+vector<int32> v;
+mutex m;
 
-void Add() {
-	for (int32 i = 0; i < 100'0000; ++i)
+void Push()
+{
+	for (int32 i = 0; i < 100'000; ++i)
 	{
-		sum.fetch_add(1);
-	}
-}
-
-void Sub() {
-	for (int32 i = 0; i < 100'0000; ++i)
-	{
-		sum.fetch_add(-1);
+		m.lock();
+		v.emplace_back(i);
+		m.unlock();
 	}
 }
 
 int main()
 {   
-	Add();
-	Sub();
-	cout << sum << endl;
+	thread t1(Push);
+	thread t2(Push);
 
-	std::thread t1(Add);
-	std::thread t2(Sub);
 	t1.join();
 	t2.join();
-
-	cout << sum << endl;
 }
