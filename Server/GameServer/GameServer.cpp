6433f7cbf4f6114aadb642dd73ea229bd4962246
DeukYu <ldy8070@naver.com>
2024-05-20 21:27:39 +0900

    Update ServerCore.vcxproj.filters

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,42 +4,8 @@
 #include <CoreMacro.h>
 #include <ThreadManager.h>
 
-class Wraight
-{
-public:
-	int mHp = 150;
-	int mPosX = 0;
-	int mPosY = 0;
-};
-
-class Missile
-{
-public:
-	void SetTarget(Wraight* target)
-	{
-		_target = target;
-	}
-
-	void Update() {
-		int PosX = _target->mPosX;
-		int PosY = _target->mPosY;
-	}
-
-	Wraight* _target = nullptr;
-};
 
 int main()
 {   
-	Wraight* wraight = new Wraight();
-	Missile* missile = new Missile();
-	missile->SetTarget(wraight);
 
-	wraight->mHp = 0;
-	delete wraight;
-
-	while (true)
-	{
-		missile->Update();
-	}
-	delete missile;
 }
