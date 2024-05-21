453032151a6d97f9ff73401400dcb76f4fe45d74
DeukYu <ldy8070@naver.com>
2024-05-21 22:50:52 +0900

    Update GameServer.cpp

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,8 +4,18 @@
 #include <CoreMacro.h>
 #include <ThreadManager.h>
 
+class Knight
+{
+public:
+	Knight() {
+		cout << "Knight()" << endl;
+	}
+	~Knight() {
+		cout << "~Knight()" << endl;
+	}
+};
 
 int main()
 {   
-
+	shared_ptr<Knight> spr = make_shared<Knight>();
 }
