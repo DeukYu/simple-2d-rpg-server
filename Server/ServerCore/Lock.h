d03f3b657f10809a352cb4da12e467b6b4f722dd
DeukYu <ldy8070@naver.com>
2024-05-23 01:28:44 +0900

    코드 리펙토링 작업

--- Server/ServerCore/Lock.h
+++ Server/ServerCore/Lock.h
@@ -4,30 +4,30 @@
 class Lock
 {
 public:
-	void	WriteLock(string_view name);
-	void	WriteUnlock(string_view name);
-	void	ReadLock(string_view name);
-	void	ReadUnlock(string_view name);
+    void WriteLock(string_view name);
+    void WriteUnlock(string_view name);
+    void ReadLock(string_view name);
+    void ReadUnlock(string_view name);
 private:
-	shared_timed_mutex mMutex;
+    shared_timed_mutex mMutex;
 };
 
 class ReadLockGuard
 {
 public:
-	ReadLockGuard(Lock& lock, string_view name) : mLock(lock), mName(name) { mLock.ReadLock(name); }
-	~ReadLockGuard() { mLock.ReadUnlock(mName); }
+    ReadLockGuard(Lock& lock, string_view name) : mLock(lock), mName(name) { mLock.ReadLock(name); }
+    ~ReadLockGuard() { mLock.ReadUnlock(mName); }
 private:
-	Lock& mLock;
-	string mName;
+    Lock& mLock;
+    string mName;
 };
 
 class WriteLockGuard
 {
 public:
-	WriteLockGuard(Lock& lock, string_view name) : mLock(lock), mName(name) { mLock.WriteLock(name); }
-	~WriteLockGuard() { mLock.WriteUnlock(mName); }
+    WriteLockGuard(Lock& lock, string_view name) : mLock(lock), mName(name) { mLock.WriteLock(name); }
+    ~WriteLockGuard() { mLock.WriteUnlock(mName); }
 private:
-	Lock& mLock;
-	string mName;
+    Lock& mLock;
+    string mName;
 };
