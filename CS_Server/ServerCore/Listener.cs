a13a712103c261832eadd25b8812d238751d044d
DeukYu <ldy8070@naver.com>
2024-08-18 23:04:04 +0900

    Create Listener.cs

---
+++ CS_Server/ServerCore/Listener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listenSocket;
+
+        public void Init(IPEndPoint endPoint)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            _listenSocket.Bind(endPoint);
+
+            _listenSocket.Listen(10);
+        }
+
+        public Socket Accept()
+        {
+            _listenSocket.AcceptAsync();
+
+            return _listenSocket.Accept();
+        }
+    }
+}
