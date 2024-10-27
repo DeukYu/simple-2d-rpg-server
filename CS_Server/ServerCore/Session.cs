16b167cdadcd08ac6fe7ad1adfee90329e20bb7d
이득유 <ldy8070@naver.com>
2024-10-27 21:21:07 +0900

    코드 수정

--- CS_Server/ServerCore/Session.cs
+++ CS_Server/ServerCore/Session.cs
@@ -1,91 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ServerCore
+namespace ServerCore;
+
+public class Session
 {
-    public class Session
+    Socket _socket;
+    int _disconnected = 0;
+
+    public void Initialize(Socket socket)
     {
-        Socket _socket;
-        int _disconnected = 0;
+        _socket = socket;
+        SocketAsyncEventArgs receiveArgs = new SocketAsyncEventArgs();
+        receiveArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
+        receiveArgs.SetBuffer(new byte[1024], 0, 1024);
 
-        public void Initialize(Socket socket)
-        {
-            _socket = socket;
-            SocketAsyncEventArgs receiveArgs = new SocketAsyncEventArgs();
-            receiveArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
-            receiveArgs.SetBuffer(new byte[1024], 0, 1024);
-
-            RegisterReceive(receiveArgs);
-        }
-
-        public void Send(byte[] sendBuff)
-        {
-           _socket.Send(sendBuff);
-        }
-
-        public void Disconnect()
-        {
-            // 이미 끊겼다면 return (두번의 Disconnect를 방지)
-            if (Interlocked.Exchange(ref _disconnected, 1) == 1)
-                return;
-
-            _socket.Shutdown(SocketShutdown.Both);
-            _socket.Close();
-        }
-
-        #region Network communication
-        private void RegisterSend(SocketAsyncEventArgs args)
-        {
-            bool pending = _socket.SendAsync(args);
-            if(pending == false)
-            {
-                OnSendCompleted(null, args);
-            }
-        }
-        private void OnSendCompleted(object sender, SocketAsyncEventArgs args)
-        {
-            if(args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
-            {
-                Console.WriteLine($"Transferred bytes: {args.BytesTransferred}");
-                RegisterSend(args);
-            }
-            else
-            {
-                Disconnect();
-            }
-        }
-        private void RegisterReceive(SocketAsyncEventArgs args)
-        {
-            bool pending = _socket.ReceiveAsync(args);
-            if(pending == false)
-            {
-                OnReceiveCompleted(null, args);
-            }
-        }
-        private void OnReceiveCompleted(object sender, SocketAsyncEventArgs args)
-        {
-            if(args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
-            {
-                try
-                {
-                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
-                    Console.WriteLine($"[From Client] {recvData}");
-                    RegisterReceive(args);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"OnReceiveCompleted Failed {e}");
-                }
-            }
-            else
-            {
-                Disconnect();
-            }
-        }
-        #endregion
+        RegisterReceive(receiveArgs);
     }
+
+    public void Send(byte[] sendBuff)
+    {
+        _socket.Send(sendBuff);
+    }
+
+    public void Disconnect()
+    {
+        // 이미 끊겼다면 return (두번의 Disconnect를 방지)
+        if (Interlocked.Exchange(ref _disconnected, 1) == 1)
+            return;
+
+        _socket.Shutdown(SocketShutdown.Both);
+        _socket.Close();
+    }
+
+    #region Network communication
+    private void RegisterSend(SocketAsyncEventArgs args)
+    {
+        bool pending = _socket.SendAsync(args);
+        if (pending == false)
+        {
+            OnSendCompleted(null, args);
+        }
+    }
+    private void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+    {
+        if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+        {
+            Console.WriteLine($"Transferred bytes: {args.BytesTransferred}");
+            RegisterSend(args);
+        }
+        else
+        {
+            Disconnect();
+        }
+    }
+    private void RegisterReceive(SocketAsyncEventArgs args)
+    {
+        bool pending = _socket.ReceiveAsync(args);
+        if (pending == false)
+        {
+            OnReceiveCompleted(null, args);
+        }
+    }
+    private void OnReceiveCompleted(object sender, SocketAsyncEventArgs args)
+    {
+        if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+        {
+            try
+            {
+                string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+                Console.WriteLine($"[From Client] {recvData}");
+                RegisterReceive(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"OnReceiveCompleted Failed {e}");
+            }
+        }
+        else
+        {
+            Disconnect();
+        }
+    }
+    #endregion
 }
