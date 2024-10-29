4344a210fcbfc23bdea6ff2dfe98926dbfc52883
이득유 <ldy8070@naver.com>
2024-10-30 03:38:24 +0900

    [CS_Server] DnsUtil 로 localIpAddr 공통 함수 만들어 사용

--- CS_Server/CS_Server/Program.cs
+++ CS_Server/CS_Server/Program.cs
@@ -39,9 +39,7 @@ class Program
     static void Main(string[] args)
     {
         // DNS (Domain Name System)
-        string host = Dns.GetHostName();
-        IPHostEntry ipHost = Dns.GetHostEntry(host);
-        IPAddress ipAddr = ipHost.AddressList[0];
+        IPAddress ipAddr = DnsUtil.GetLocalIpAddress(); 
         IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
         _listener.Initialize(endPoint, () => { return new GameSession(); });
---
+++ CS_Server/ServerCore/DnsUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace ServerCore;
+
+public static class DnsUtil
+{
+    public static IPAddress GetLocalIpAddress()
+    {
+        string hostName = Dns.GetHostName();
+        var ipHost = Dns.GetHostEntry(hostName);
+        foreach (var ipAddr in ipHost.AddressList)
+        {
+            if (ipAddr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                return ipAddr;
+            }
+        }
+
+        throw new Exception("Local ip address not found.");
+    }
+}
