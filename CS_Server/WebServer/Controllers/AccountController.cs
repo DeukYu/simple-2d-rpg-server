9b3244cdd906bfb974c0808a14a74bb870efa0b9
이득유 <ldy8070@naver.com>
2024-11-04 00:28:47 +0900

    [CS_Server] Login 부분을 웹서버로 처리함으로써 게임 서버 로직단에서 삭제

--- CS_Server/WebServer/Controllers/AccountController.cs
+++ CS_Server/WebServer/Controllers/AccountController.cs
@@ -10,9 +10,11 @@ public class AccountController : ControllerBase
 {
     [HttpGet]
     [Route("login")]
-    public LoginAccountRes LoginAccount(LoginAccountReq id)
+    public LoginAccountRes LoginAccount(LoginAccountReq req)
     {
         // TODO : 현재 DB가 없는 상태에선 무조건 OK를 보낸다.
+        // TODO : 나중에 DB 연동하면서 로직을 변경해야 한다.
+        // TODO : 로그인 계정이 존재하지 않더라도 계정을 생성하고 OK 패킷을 보내도록 처리한다.
         return new LoginAccountRes
         {
             Result = (int)ErrorType.Success
