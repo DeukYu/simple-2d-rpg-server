8f78acff094aff0e1381675279c71a1829dc74f9
이득유 <ldy8070@naver.com>
2024-11-18 02:35:29 +0900

    Job 방식 변경

--- CS_Server/CS_Server/Job/JobSerializer.cs
+++ CS_Server/CS_Server/Job/JobSerializer.cs
@@ -8,10 +8,10 @@ public class JobSerializer
     private ConcurrentQueue<IJob> _jobQueue = new ConcurrentQueue<IJob>();
     private AtomicFlag _flush = new AtomicFlag();
 
-    public void Push(Action action) { Push(new Job(action)); }
-    public void Push<T1>(Action<T1> action, T1 t1) { Push(new Job<T1>(action, t1)); }
-    public void Push<T1, T2>(Action<T1, T2> action, T1 t1, T2 t2) { Push(new Job<T1, T2>(action, t1, t2)); }
-    public void Push<T1, T2, T3>(Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3) { Push(new Job<T1, T2, T3>(action, t1, t2, t3)); }
+    public void Push(Delegate action, params object[] parameters)
+    {
+        Push(new Job(action, parameters));
+    }
 
     public void Push(IJob job)
     {
