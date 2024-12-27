13825635f0f6ecea18102dc0907f97db6ecbcd96
LeeDeukYu <ldy8070@naver.com>
2024-12-27 17:19:04 +0900

    GenPacket 수정

--- CS_Server/Common/Packet/GenPackets.bat
+++ CS_Server/Common/Packet/GenPackets.bat
@@ -3,20 +3,20 @@ IF ERRORLEVEL 1 PAUSE
 
 REM START ../../PacketGenerator/bin/Debug/PacketGenerator.exe ../../PacketGenerator/Protocol.proto
 IF ERRORLEVEL 1 PAUSE
-XCOPY /Y Protocol.cs "../../../../simple-2d-rpg-client/Assets/03_Scripts/Packet"
+XCOPY /Y Protocol.cs "../../../../simple-2d-rpg-client/Assets/Scripts/Packet"
 XCOPY /Y Protocol.cs "../../CS_Server/Packet"
 XCOPY /Y Protocol.cs "../../DummyClient/Packet"
 
-XCOPY /Y Common.cs "../../../../simple-2d-rpg-client/Assets/03_Scripts/Packet"
+XCOPY /Y Common.cs "../../../../simple-2d-rpg-client/Assets/Scripts/Packet"
 REM XCOPY /Y Common.cs "../../CS_Server/Packet"
 REM XCOPY /Y Common.cs "../../WebServer/Packet"
 XCOPY /Y Common.cs "../../Shared"
 
-XCOPY /Y Enum.cs "../../../../simple-2d-rpg-client/Assets/03_Scripts/Packet"
+XCOPY /Y Enum.cs "../../../../simple-2d-rpg-client/Assets/Scripts/Packet"
 XCOPY /Y Enum.cs "../../Shared"
 
-XCOPY /Y WebProtocol.cs "../../../../simple-2d-rpg-client/Assets/03_Scripts/Packet"
+XCOPY /Y WebProtocol.cs "../../../../simple-2d-rpg-client/Assets/Scripts/Packet"
 XCOPY /Y WebProtocol.cs "../../WebServer/Packet"
 XCOPY /Y WebProtocol.cs "../../DummyClient/Packet"
-REM XCOPY /Y ClientPacketManager.cs "../../../../GuardianOfNature2D/Assets/03_Scripts/Packet"
+REM XCOPY /Y ClientPacketManager.cs "../../../../GuardianOfNature2D/Assets/Scripts/Packet"
 REM XCOPY /Y ServerPacketManager.cs "../../CS_Server/Packet"
